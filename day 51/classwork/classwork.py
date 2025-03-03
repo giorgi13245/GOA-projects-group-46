f1b70a5dc07fcd3d81bbdc6c05170dd57f2bba15
giorgi13245 <giorgibokoveli10@gmail.com>
2025-03-03 20:04:15 +0400

    second commit

---
+++ day 51/classwork/classwork.py
@@ -0,0 +1,22 @@
+#list ისგან განსხვავებით tuple არის immutable რაც ნიშნავს იმას რომ ის უცვლელია და მასზე არ მუშაობს append,insert,pop,remove  და ა.შ.
+
+
+films=("film1","film2","film3","film4","film5")
+print(films[0:5:4])
+
+
+days=("monday","tuesday","wednesday","thursday","friday","saturday","sunday")
+(a, b, c, d, e, f, g)= days
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
+random=(1,2,3,4,5,6,7,8,9)
+print(10 in random)
+
+
